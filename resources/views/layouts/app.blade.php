64acc282caa348a9a9f52d70e223f24a26018c75
unknown <gilbert_kenneth04@yahoo.com>
2018-10-23 17:21:46 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,7 +126,7 @@
                         <a class="nav-link" href="{{ route('settings') }}">Settings</a>
                     </li>
                     </ul>
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto text-center">
                     <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
